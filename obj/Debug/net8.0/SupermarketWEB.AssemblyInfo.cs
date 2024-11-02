e4bdf9fd69913d4772ad2ace53b98620ed7e8bfa
Jose Leonardo Soto <jsotto2003@gmail.com>
2024-11-02 01:05:43 -0500

    Delete & Edit of Products

--- obj/Debug/net8.0/SupermarketWEB.AssemblyInfo.cs
+++ obj/Debug/net8.0/SupermarketWEB.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SupermarketWEB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+196ee26295942cada5bf4e8673384e93820a1049")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee269752ac37a84fe0ccc1c26cbc51764790a310")]
 [assembly: System.Reflection.AssemblyProductAttribute("SupermarketWEB")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SupermarketWEB")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
