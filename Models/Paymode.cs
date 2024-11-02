e4db8acd2f14683dade599ab41d197af0062d892
Jose Leonardo Soto <jsotto2003@gmail.com>
2024-11-02 01:36:54 -0500

    Products Page Finished

---
+++ Models/Paymode.cs
@@ -0,0 +1,15 @@
+﻿namespace SupermarketWEB.Models
+{
+
+    public class Paymode
+        {
+
+        public int Id { get; set; } // Será la llave primaria
+
+        public string Name { get; set; }
+
+        public string? Observation { get; set; }
+
+        public ICollection<Paymode>? Paymodes { get; set; } = default!;// Propiedad de navegación
+        }
+}
